73bee8e1ee33ddd25b7500e22f243a5c39cdbec2
resqiar <resqiageng@gmail.com>
2022-08-19 14:29:03 +0700

    feat: show data from the backend

---
+++ constants/url.ts
@@ -0,0 +1,3 @@
+export const C_URL = {
+  BACKEND_URL: "https://2769-36-68-219-108.ap.ngrok.io",
+};
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,11 +1,33 @@
+import { useEffect, useState } from "react";
 import MainStatusBar from "../components/miscs/MainStatusBar";
 import ExtensionCard from "../components/card/ExtensionCard";
 import HomeHeader from "../components/header/HomeHeader";
 import { FlatList, View } from "react-native";
 import { NavigationType } from "../types/navigation";
-import { mockup } from "../constants/mockup";
+import { C_URL } from "../constants/url";
+import { IData } from "../constants/data";
 
 export default function HomeScreen(props: NavigationType<"HomeScreen">) {
+  // State to store backend request data
+  const [data, setData] = useState<IData[]>([]);
+
+  async function fetchData() {
+    try {
+      // TIMEOUT TO SIMULATE NETWORK REQUEST
+      setTimeout(async () => {
+        const req = await fetch(`${C_URL.BACKEND_URL}/ext`);
+        const res = await req.json();
+        setData(res);
+      }, 5000);
+    } catch (error: any) {
+      console.error(error.message);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <View className="flex-1">
       {/* CUSTOM STATUS BAR */}
@@ -16,7 +38,7 @@ export default function HomeScreen(props: NavigationType<"HomeScreen">) {
         {/* CARD COMPONENT LIST */}
         <View className="z-10">
           <FlatList
-            data={mockup}
+            data={data}
             renderItem={({ item }) => <ExtensionCard rn={props} value={item} />}
             keyExtractor={(i) => i.id.toString()}
             ListHeaderComponent={HomeHeader}
