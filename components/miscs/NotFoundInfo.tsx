f2abf8e3cc2d58d38b74e8169de0e3ab94db4482
resqiar <resqiageng@gmail.com>
2022-08-19 17:08:55 +0700

    style(NotFoundInfo): fix style issue on device's light-mode

--- components/miscs/NotFoundInfo.tsx
+++ components/miscs/NotFoundInfo.tsx
@@ -3,14 +3,13 @@ import { Entypo } from "@expo/vector-icons";
 
 export default function NotFoundInfo() {
   return (
-    <View className="items-center mx-2 my-4 p-4">
+    <View className="items-center bg-white shadow-2xl rounded-xl mx-2 my-4 p-4">
       <View className="py-4">
-        <Entypo name="emoji-sad" size={150} />
+        <Entypo name="emoji-sad" size={150} color="rgb(75, 85, 99)" />
       </View>
       <Text className="text-2xl font-bold">Extension Not Found</Text>
       <Text className="text-center py-3">
-        Try to check the id of the extension, the id might be wrong or
-        misspelled.
+        Try check the id of the extension, the id might be wrong or misspelled.
       </Text>
     </View>
   );
