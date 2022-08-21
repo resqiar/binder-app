c91ac1f662ec8f6fb9122766e4a98b26c67de277
resqiar <resqiageng@gmail.com>
2022-08-21 22:03:16 +0700

    feat: support deep linking url

--- screens/StackScreen.tsx
+++ screens/StackScreen.tsx
@@ -1,16 +1,31 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { LinkingOptions, NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StackList } from "../types/stack";
 import DetailScreen from "../screens/DetailScreen";
 import HomeScreen from "../screens/HomeScreen";
 import AboutScreen from "./AboutScreen";
 import AboutHeaderRight from "../components/header/AboutHeaderRight";
+import * as Linking from "expo-linking";
 
 const Stack = createNativeStackNavigator<StackList>();
 
+// Prefix linking
+const prefixLinking = Linking.createURL("/");
+
 export default function StackScreen() {
+  // LinkingOptions passes into NavigationContainer
+  const link: LinkingOptions<StackList> = {
+    prefixes: [prefixLinking],
+    config: {
+      screens: {
+        HomeScreen: "home",
+        AboutScreen: "about",
+      },
+    },
+  };
+
   return (
-    <NavigationContainer>
+    <NavigationContainer linking={link}>
       <Stack.Navigator
         screenOptions={{
           headerShown: false,
